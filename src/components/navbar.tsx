a2cbcf0e8e6cd5d28ee8976d9178a75506865512
akeemkabiru <kabby.dev@outlook.com>
2024-12-06 18:30:48 +0100

    navBar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { NAVBAR_ITEMS } from "@/utils/constant";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function NavBar() {
+  const pathName = usePathname();
+  const { push } = useRouter();
+
+  return (
+    <main className="w-full flex items-center justify-between fixed px-16 py-8">
+      <h2 className="font-bold text-3xl">Kabby</h2>
+      <nav className="flex items-center gap-8 justify-between text-sm font-semibold bg-white/30 rounded-lg shadow-2xl backdrop-blur-[16.4px] border border-white/30 px-16 py-4">
+        {NAVBAR_ITEMS.map(({ value, route }, index: number) => {
+          const isActive = pathName.endsWith(route);
+
+          return (
+            <h3
+              key={index}
+              onClick={() => push(route)}
+              className={`cursor-pointer hover:text-primary transition-all duration-500 ${
+                isActive ? "text-primary " : ""
+              }`}
+            >
+              {value}
+            </h3>
+          );
+        })}
+      </nav>
+      <button className="bg-gradient-to-r text-sm transition-all duration-300 animate-pulse from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md">
+        Resume
+      </button>
+    </main>
+  );
+}
---
+++ src/utils/constant.ts
@@ -0,0 +1,7 @@
+export const NAVBAR_ITEMS = [
+  { value: "Home", route: "/" },
+  { value: "About", route: "/about" },
+  { value: "Experience", route: "/experience" },
+  { value: "Projects", route: "/projects" },
+  { value: "Contact", route: "/contact" },
+];
