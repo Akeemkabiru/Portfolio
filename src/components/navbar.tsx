865c03877d2c696e3b74e9162943f2750b837460
akeemkabiru <kabby.dev@outlook.com>
2024-12-06 18:47:08 +0100

    completed navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,9 @@ export default function NavBar() {
 
   return (
     <main className="w-full flex items-center justify-between fixed px-16 py-8">
-      <h2 className="font-bold text-3xl">Kabby</h2>
+      <h2 className="font-bold text-xl border px-3 py-2 rounded-lg  border-primary">
+        K
+      </h2>
       <nav className="flex items-center gap-8 justify-between text-sm font-semibold bg-white/30 rounded-lg shadow-2xl backdrop-blur-[16.4px] border border-white/30 px-16 py-4">
         {NAVBAR_ITEMS.map(({ value, route }, index: number) => {
           const isActive = pathName.endsWith(route);
