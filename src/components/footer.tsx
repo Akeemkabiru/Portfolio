04094867d8e9100d42a75ed29947ce70f8b884f3
akeemkabiru <kabby.dev@outlook.com>
2024-12-10 19:48:11 +0100

    fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <section className="bg-background flex flex-col items-center justify-center w-full py-4 border border-gray-900 gap-4">
+    <section className="bg-background flex flex-col items-center justify-center w-full py-4 gap-4">
       <div className="flex items-center justify-center gap-8">
         <FaArrowLeftLong size={25} />
         <FaArrowRightLong size={25} />
