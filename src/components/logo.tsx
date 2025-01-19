6f720a9711beb9144d220cab05e96a71d13d2eb5
akeemkabiru <kabby.dev@outlook.com>
2025-01-19 17:54:51 +0100

    fix issue

--- src/components/logo.tsx
+++
@@ -1,3 +0,0 @@
-export default function Logo() {
-  return <div className=" border-primary text-xl font-bold">Kabby</div>;
-}
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,17 +1,8 @@
-import { BsLinkedin } from "react-icons/bs";
-
 export const NAVBAR_ITEMS = [
   { value: "Home", route: "/" },
   { value: "Projects", route: "/about" },
   { value: "About", route: "/experience" },
   { value: "Contact", route: "/contact" },
-];
-
-export const SOCIALS: { link: string; icon: any }[] = [
-  // { link: "https://github.com/Akeemkabiru", icon: "/github.svg" },
-  { link: "https://www.linkedin.com/in/akeemkabiru", icon: BsLinkedin },
-  // { link: "https://x.com/kabby_10", icon: "/x.svg" },
-  // { link: "https://x.com/kabby_10", icon: "/x.svg" },
 ];
 
 export const SKILLS_ITEM = [
