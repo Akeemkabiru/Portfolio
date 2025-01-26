821a02779bb38a64f442033d04497530053b83f5
akeemkabiru <kabby.dev@outlook.com>
2025-01-26 22:00:29 +0100

    added refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ import Projects from "@/components/project";
 
 export default function Home() {
   return (
-    <>
+    <main className="px-6 md:px-12 lg:px-16 lg:pt-0 pt-12 md:pt-24">
       <Hero />
       <Projects />
       <Experience />
       <Copyright />
-    </>
+    </main>
   );
 }
