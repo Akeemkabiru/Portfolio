04b9d79de120c9f5d922974d4b883955acfde9eb
akeemkabiru <kabby.dev@outlook.com>
2025-02-22 03:44:39 +0100

    fixes

--- src/service/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "url",
-  timeout: 1000,
-  headers: { "Content-Type": "application/json", Accept: "" },
-});
-
-API.interceptors.request.use(() => {});
